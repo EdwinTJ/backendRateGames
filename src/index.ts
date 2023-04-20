e77368e5f989c3c48d2b5664082f9b1dadf3348f
EdwinTJ <edwin16x@gmail.com>
2023-04-19 20:33:10 -0600

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,12 @@ import { commentRoutes } from "./routes/commentRoutes";
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 //ROUTES
 app.use("/api/posts", postRoutes);
