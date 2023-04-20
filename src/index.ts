3182f2ce932b6836f083f809fd290ba96c4d1f95
EdwinTJ <edwin16x@gmail.com>
2023-04-19 20:17:13 -0600

    Add Cors and port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+const PORT = process.env.PORT || 8000;
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log("Server started on port 3000");
 });
